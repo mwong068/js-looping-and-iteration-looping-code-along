b153ca20bf8f815f2af66a4c2cf217d92cfb2315
mwong068 <mwong068@gmail.com>
2019-12-09 15:45:01 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    let messages = [];
+    for (let i = 0; i < array.length; i++) {
+        messages.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+function countDown(num) {
+    for(let i = num; i >= 0; i--) {
+        console.log(i);
+    }
+}
